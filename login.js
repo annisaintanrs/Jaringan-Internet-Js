88d5ca8a4a204b8bc7701f2c7996512bd2858be2
Annisaintanrs <165027538+annisaintanrs@users.noreply.github.com>
2024-06-01 18:50:13 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,22 +1,14 @@
-// Mengaktifkan tombol login
 document.getElementById('login-btn').addEventListener('click', function() {
-    // Mendapatkan nilai input username dan password
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // Mengecek apakah input username dan password tidak kosong
     if (username !== '' && password !== '') {
-        // Mengecek apakah username dan password sesuai dengan data yang tersimpan
-        // (dalam contoh ini, saya tidak menggunakan data yang tersimpan, jadi Anda harus menggantikan dengan kode yang sesuai)
         if (username === 'admin' && password === 'password') {
-            // Jika sesuai, maka login berhasil
             alert('Login Berhasil!');
         } else {
-            // Jika tidak sesuai, maka login gagal
             alert('Login Gagal!');
         }
     } else {
-        // Jika input username dan password kosong, maka login gagal
         alert('Username dan Password tidak boleh kosong!');
     }
 });
